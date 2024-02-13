78c49ac72c7c58b273196ffcce54ac35bcbf0052
Jeffrey Miller <jeffrey.miller664@gmail.com>
2024-02-13 16:11:24 +0200

    Refactor build options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@
 export default {
   // config options
   base: "/",
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].js",
-        assetFileNames: "assets/[name][extname]",
-      },
-    },
-  },
 };
